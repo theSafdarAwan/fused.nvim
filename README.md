cab339c1a5101f883b35be50ccda57d6f3f36948
TheSafdarAwan <thesafdarawan@gmail.com>
2022-11-21 12:44:09 +0500

    update

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # fused.nvim
+
 A colorscheme fused between different color schemes
+
+## TO-DO
+
+- Add Higlights
+  - Neogit
+  - Diffview
+  - Packer
+  - cmp(add the remaining higlights)
+  - vim-matchup
+- Add the style option
--- lua/fused/init.lua
+++ lua/fused/init.lua
@@ -4,7 +4,7 @@ local defaults = require("fused.utils").default_config
 
 M.setup = function(tbl)
     local conf
-    if tbl.flavour or tbl.plugins_integration then
+    if tbl.flavour or tbl.plugins_integration or tbl.custom then
         conf = tbl
     else
         conf = defaults
