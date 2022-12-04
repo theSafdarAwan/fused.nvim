53bbf9cfc54ed1050fea27edd6952040133494cc
TheSafdarAwan <thesafdarawan@gmail.com>
2022-12-05 02:50:25 +0500

    Add the autocmd for add the cursorline to the nvimtree

--- after/plugin/fused.lua
+++ after/plugin/fused.lua
@@ -3,6 +3,8 @@ This is just to solve the problem on norg not showing the colors for the prefixe
 --]]
 local vim = vim
 local api = vim.api
+local opt = vim.opt_local
+local b = vim.bo
 
 -- creating a augroup for the fused
 local au_fused = api.nvim_create_augroup("fused", { clear = true })
@@ -16,4 +18,18 @@ api.nvim_create_autocmd({ "FileType" }, {
 	group = au_fused,
 	pattern = "norg",
 	callback = neorg_opts,
+})
+
+-- set the cursorline for the nvimtree
+local function nvim_tree()
+	if b.filetype == "NvimTree" then
+		opt.cursorline = true
+	end
+end
+
+api.nvim_create_autocmd({ "BufWinEnter" }, {
+	group = au_fused,
+	callback = function()
+		nvim_tree()
+	end,
 })
