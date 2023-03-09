372b2776f88b6b31ff76af3324cdf02ca838ceb8
TheSafdarAwan <thesafdarawan@gmail.com>
2023-03-09 17:26:29 +0500

    feat: added the palenight pallete

---
+++ lua/fused/pallets/palenight.lua
@@ -0,0 +1,63 @@
+local colors = {
+	color_1 = "#252931",
+	color_2 = "#333748",
+	color_3 = "#3c4051",
+	color_4 = "#4c5374",
+	color_5 = "#606475",
+	color_6 = "#929ac9",
+	color_7 = "#bfc7d5",
+
+	color_8 = "#f07178",
+	color_9 = "#FFCB6B",
+	color_10 = "#82AAFF",
+
+	color_11 = "#ff5572",
+	color_12 = "#e2b93d",
+	color_13 = "#89ffe6",
+
+	color_14 = "#d1aaff",
+	color_15 = "#7e57c2",
+	color_16 = "#ffcb8b",
+	color_17 = "#8fb7ff",
+	color_18 = "#C3E88D",
+
+	color_19 = "#9CCC65",
+	color_20 = "#64B5F6",
+	color_21 = "#ffa282",
+	color_22 = "#89DDFF",
+	color_23 = "#EF5350",
+}
+
+local M = {}
+
+M.pallete = {
+	base01 = colors.color_1,
+	base02 = colors.color_2,
+	base03 = colors.color_3,
+	base04 = colors.color_4,
+	base05 = colors.color_5,
+	base06 = colors.color_6,
+	base07 = colors.color_7,
+
+	base08 = colors.color_8,
+	base09 = colors.color_9,
+	base10 = colors.color_10,
+
+	base11 = colors.color_11,
+	base12 = colors.color_12,
+	base13 = colors.color_13,
+
+	base14 = colors.color_14,
+	base15 = colors.color_15,
+	base16 = colors.color_16,
+	base17 = colors.color_17,
+	base18 = colors.color_18,
+
+	base19 = colors.color_19,
+	base20 = colors.color_20,
+	base21 = colors.color_21,
+	base22 = colors.color_22,
+	base23 = colors.color_23,
+}
+
+return M
