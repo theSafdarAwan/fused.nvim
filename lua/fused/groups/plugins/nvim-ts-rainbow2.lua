1f0687f030fc3a1e8f99e6139aa89ef46f817a74
TheSafdarAwan <thesafdarawan@gmail.com>
2023-03-18 22:37:34 +0500

    fix: fixed the nvim-ts-rainbow2 colors

--- lua/fused/groups/plugins/nvim-ts-rainbow2.lua
+++ lua/fused/groups/plugins/nvim-ts-rainbow2.lua
@@ -2,14 +2,13 @@ local M = {}
 
 M.get_hl_groups = function(cp)
 	return {
-		rainbowcol1 = { fg = cp.base22 },
-		rainbowcol2 = { fg = cp.base20 },
-		rainbowcol3 = { fg = cp.base23 },
-		rainbowcol4 = { fg = cp.base19 },
-		rainbowcol5 = { fg = cp.ts_pruple },
-		rainbowcol6 = { fg = cp.base22 },
-		rainbowcol7 = { fg = cp.base20 },
-		rainbowcol8 = { fg = cp.base23 },
+		TSRainbowRed = { fg = cp.base11 },
+		TSRainbowBlue = { fg = cp.base14 },
+		TSRainbowCyan = { fg = cp.base09 },
+		TSRainbowGreen = { fg = cp.base17 },
+		TSRainbowOrange = { fg = cp.base18 },
+		TSRainbowViolet = { fg = cp.base13 },
+		TSRainbowYellow = { fg = cp.base10 },
 	}
 end
 
