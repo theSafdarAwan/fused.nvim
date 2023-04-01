e17b474c483c8a03703c7ab323051d6542d086ab
TheSafdarAwan <thesafdarawan@gmail.com>
2023-04-01 17:50:36 +0500

    extend: added new hl groups for neo-minimap

--- lua/fused/groups/plugins/neo-minimap.lua
+++ lua/fused/groups/plugins/neo-minimap.lua
@@ -3,6 +3,7 @@ local M = {}
 M.get_hl_groups = function(cp)
 	return {
 		NeoMinimapBorder = { link = "FloatBorder" },
+		NeoMinimapLineNr = { link = "LineNr" },
 		NeoMinimapBackground = { link = "Normal" },
 		NeoMinimapCursorLine = { link = "CursorLine" },
 	}
